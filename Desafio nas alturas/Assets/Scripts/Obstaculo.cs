6393567ec2f024c329e849ff27c0e56feacc8156
Felipe <felipesantos-01@hotmail.com>
2023-10-16 20:14:49 -0300

    pasta com os códigos

---
+++ Desafio nas alturas/Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour {
+    [SerializeField]
+    private float velocidade = 0.5f;
+    [SerializeField]
+    private float variacaoDaPosicaoY;
+    private Vector3 posicaoDoAviao;
+    private bool pontuei;
+    private Pontuacao pontuacao;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoDaPosicaoY, variacaoDaPosicaoY));
+    }
+
+    private void Start()
+    {
+        this.posicaoDoAviao = GameObject.FindObjectOfType<Aviao>().transform.position;
+        this.pontuacao = GameObject.FindObjectOfType<Pontuacao>();
+
+    }
+
+    private void Update () {
+        this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+
+        if (!this.pontuei && this.transform.position.x < this.posicaoDoAviao.x)
+        {
+            Debug.Log("pontuou");
+            this.pontuei = true;
+            this.pontuacao.AdicionarPontos();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D outro)
+    {
+        this.Destruir();
+    }
+
+    public void Destruir()
+    {
+        Destroy(this.gameObject);
+    }
+}
